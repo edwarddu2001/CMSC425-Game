75b8c8360b9876f1b4620ff44b6934a7726bfb3e
bcantler2389 <bbblaircantler89@gmail.com>
2022-11-29 15:03:02 -0500

    Update InvisObject to enable/disable entire obj

--- Assets/Scripts/InvisibleObject.cs
+++ Assets/Scripts/InvisibleObject.cs
@@ -7,17 +7,24 @@ public class InvisibleObject : MonoBehaviour
     [SerializeField]
     private LightUpAbility lightUpAbility;
     private MeshRenderer mesh;
+    private GameObject obj;
     void Start(){
         mesh = GetComponent<MeshRenderer>();
+        obj = GameObject.Find("InvisCube");
+        //make sure that any object you want to "light up" are called "InvisCube" 
+        //the name must be exact for it to work
+
         //make sure there is a controller for this ability in the scene
         if (lightUpAbility != null){
             lightUpAbility.ReportLit += MakeVisible;
         }
         mesh.enabled = false;
+        obj.SetActive(false);
     
     }
 
     void MakeVisible(bool visible){
         mesh.enabled = visible;
+        obj.SetActive(true);
     }
 }

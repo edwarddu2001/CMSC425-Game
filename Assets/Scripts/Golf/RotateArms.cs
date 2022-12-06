5f71bd1bd0921adb3725ee1a81d6eccde115a567
Rami Shehadeh <rshehade@umd.edu>
2022-12-06 16:48:28 -0500

    windmill arms speed

--- Assets/Scripts/Golf/RotateArms.cs
+++ Assets/Scripts/Golf/RotateArms.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RotateArms : MonoBehaviour
 {
+    public float rotationSpeed = 80.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class RotateArms : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 80*Time.deltaTime);
+        transform.Rotate(0, 0, rotationSpeed*Time.deltaTime);
     }
 }
